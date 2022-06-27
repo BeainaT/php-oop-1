46d8c50360c6c5688180ede3359d4d50d1302b20
BeainaT <tania.df@hotmail.it>
2022-06-27 17:23:22 +0200

    added code - can't print array

--- class/movie.php
+++ class/movie.php
@@ -1,5 +1,33 @@
 <?php 
 //Set class called Movie and declaire some attributes
-//Define a construct in class
-//Define at least one method in class
+class Movie {
+    public $title;
+    public $year;
+    public $director;
+    public $MainCast = Array();
+    public $genre = Array();
 
+    //Define a construct in class
+    public function __construct($_title, $_year, $_director, $_MainCast, $_genre)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->MainCast = $_MainCast;
+        $this->director = $_director;
+        $this->genre = $_genre;
+    }
+
+    //Define at least one method in class
+    public function getCast() { //BUG
+        foreach($this->MainCast as $elm) {
+            return $elm;
+        }
+    }
+
+    public function getOld() {
+        if($this->year < 2009) {
+            return "You may have seen {$this->title} in VHS";
+        }
+        return "No VHS for {$this->title}";
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 //init at least two 'Movie' obj and print their attributes value
+require_once 'class/movie.php';
+$movies = [
+    new Movie('Hocus Pocus', 1993, 'Kenny Ortega', ['Bette Midler', 'Sarah Jessica Parker', 'Kathy Najimy', 'Omri Katz'], ['Fantasy', 'Commedy']),
+    new Movie('Mamma Mia!', 2008, 'Phyllida Lloyd', ['Meryl Streep', 'Amanda Seyfried', 'Julie Walters', 'Christine Baranski', 'Pierce Brosnan', 'Colin Firth'], ['Musical', 'Commedy']),
+    new Movie('Death Becomes Her', 1992, 'Robert Zemeckis', ['Meryl Streep', 'Goldie Hawn', 'Bruce Willis'], ['Fantasy', 'Commedy']),
+    new Movie ('Doctor Strange in the Multiverse of Madness', 2022, 'Sam Raimi', ['Benedict Cumberbatch', 'Elizabeth Olsen', 'Chiwetel Ejiofor', 'Benedict Wong', 'Xochitl Gomez'], ['Fantasy', 'Action', 'Horror']),
+    new Movie('The Devil Wears Prada', 2006, 'David Frankel', ['Meryl Streep', 'Anne Hathaway', 'Stanley Tucci', 'Adrian Grenier', 'Emily Blunt'], ['Drama', 'Commedy'])
+];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +19,14 @@
     <title>Movie-oop</title>
 </head>
 <body>
-    
+    <h1>Movies</h1>
+    <ul>
+        <?php foreach($movies as $movie) { ?>
+            <h3> <?= $movie->title ?> </h3>
+            <span> <?= "{$movie->year}: {$movie->getOld()}" ?> </span>
+            <h6> <?= $movie->director ?> </h6>
+            <h4> <?= $movie->getCast() ?> </h4> <!--can't print array-->
+            <?php } ?>
+    </ul>
 </body>
 </html>
